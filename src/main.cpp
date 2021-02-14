bcdcbd73d2c340f185166049ed0d0b33e524b0f1
Mironov <surka2012@yandex.ru>
2021-02-14 23:01:54 +0700

    added encoder stuff

--- src/main.cpp
+++ src/main.cpp
@@ -7,10 +7,17 @@
 #include <OneWire.h>
 #include <DallasTemperature.h>
 #define ONE_WIRE_BUS D3
-
+#define CLK D5
+#define DT D6
+#define SW D4
+#include "GyverEncoder.h"
+int counter;
+//Encoder enc1(CLK, DT);      // для работы без кнопки
+Encoder enc1(CLK, DT, SW);
+int previous_time;
 OneWire oneWire(ONE_WIRE_BUS);
 
-// Pass our oneWire reference to Dallas Temperature. 
+// Pass our oneWire reference to Dallas Temperature.
 DallasTemperature sensors(&oneWire);
 
 #define SCREEN_WIDTH 128 // OLED display width, in pixels
@@ -28,8 +35,8 @@ Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 #define XPOS 0 // Indexes into the 'icons' array in function below
 #define YPOS 1
 #define DELTAY 2
-
-void testdrawstyles(int var)
+float tempC;
+void testdrawstyles(int var, int count)
 {
   display.clearDisplay();
   display.setTextSize(2);              // Normal 1:1 pixel scale
@@ -37,12 +44,16 @@ void testdrawstyles(int var)
   display.setCursor(0, 0);             // Start at top-left corner
   display.print(var);
   display.println(" C");
+    display.setTextSize(1);              // Normal 1:1 pixel scale
+  display.print("counter ");
+  display.println(count);
   display.display();
-  delay(2000);
 }
 
 void setup()
 {
+  Serial.begin(115200);
+  enc1.setType(TYPE2);
   sensors.begin();
   if (!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS))
   {
@@ -50,22 +61,32 @@ void setup()
       ; // Don't proceed, loop forever
   }
   display.display();
-  delay(2000); // Pause for 2 seconds
+  delay(1000); // Pause for 2 seconds
   display.clearDisplay();
   display.drawPixel(10, 10, SSD1306_WHITE);
   display.display();
-  delay(2000);
-  testdrawstyles(variable); // Draw 'stylized' characters
+  delay(1000);
+  testdrawstyles(variable, counter); // Draw 'stylized' characters
 }
 
 void loop()
 {
-   sensors.requestTemperatures(); // Send the command to get temperatures
-   float tempC = sensors.getTempCByIndex(0);
-
-  // Check if reading was successful
-  if(tempC != DEVICE_DISCONNECTED_C) 
+  if(millis()-previous_time>3000){
+  sensors.requestTemperatures(); // Send the command to get temperatures
+  tempC = sensors.getTempCByIndex(0);
+  previous_time=millis();
+  }
+  
+  enc1.tick();
+  if (enc1.isRight())
   {
-    testdrawstyles(tempC);
-  } 
+    counter++;
+    testdrawstyles(tempC, counter);
+  } // если был поворот
+  if (enc1.isLeft())
+  {
+    counter--;
+    testdrawstyles(tempC, counter);
+  }
+
 }
