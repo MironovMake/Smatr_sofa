2e6f49eb93e2bb69885774413115d1979730ccdd
Mironov <surka2012@yandex.ru>
2021-02-25 18:13:19 +0700

    add arduino mega as a slave for esp8266 as master

--- src/WiFiAdjust.h
+++ src/WiFiAdjust.h
@@ -1,11 +1,8 @@
 
-#ifdef ESP32
-#include <WiFi.h>
-#include <AsyncTCP.h>
-#else
-#include <ESP8266WiFi.h>
-#include <ESPAsyncTCP.h>
-#endif
+  #include <ESP8266WiFi.h>
+  #include <ESPAsyncTCP.h>
+  #include <Hash.h>
+  #include <FS.h>
 #include <ESPAsyncWebServer.h>
 
 AsyncWebServer server(80);
@@ -14,8 +11,8 @@ AsyncWebServer server(80);
 const char *ssid = "Beeline_2G_F22959";
 const char *password = "dUNFcUEP";
 
-const char *parametr[]={"MIN","HOUR","TEMP","ENCODER","BUTTON"};
-String param[]={"MIN","HOUR","TEMP","ENCODER","BUTTON"};
+const char *parametr[]={"MIN","HOUR","TEMP","ENCODER","BUTTON","inputString"};
+//String param[]        ={"MIN","HOUR","TEMP","ENCODER","BUTTON"};
 
 /*
 sensor[0] minute
@@ -36,89 +33,74 @@ const char index_html[] PROGMEM = R"rawliteral(
   </script></head><body>
 
 <form action="/get" target="hidden-form">
-    Current time %MIN%:%HOUR% 
+    Current time %HOUR% : %MIN% 
   </form><br>
 
   <form action="/get" target="hidden-form">
-    inputString (current value %inputString%): <input type="text" name="inputString">
+    Send time %inputString%: <input type="text" name="inputString">
     <input type="submit" value="Submit" onclick="submitMessage()">
   </form><br>
 
 <form action="/get" target="hidden-form">
-    Encoder %Encoder%: 
+    Encoder %ENCODER%: 
   </form><br>
 
 <form action="/get" target="hidden-form">
-    TEMP %TEMP%: 
+    temperature %TEMP%: 
   </form><br>
-  
+
   <iframe style="display:none" name="hidden-form"></iframe>
 </body></html>)rawliteral";
 
 // HTML web page to handle 3 input fields (inputString, TEMP, inputFloat)
 
-void notFound(AsyncWebServerRequest *request)
-{
-    request->send(404, "text/plain", "Not found");
+void notFound(AsyncWebServerRequest *request) {
+  request->send(404, "text/plain", "Not found");
 }
 
-String readFile(fs::FS &fs, const char *path)
-{
-    //Serial.printf("Reading file: %s\r\n", path);
-    File file = fs.open(path, "r");
-    if (!file || file.isDirectory())
-    {
-        //Serial.println("- empty file or failed to open file");
-        return String();
-    }
-    //Serial.println("- read from file:");
-    String fileContent;
-    while (file.available())
-    {
-        fileContent += String((char)file.read());
-    }
-    //Serial.println(fileContent);
-    return fileContent;
+String readFile(fs::FS &fs, const char * path){
+  Serial.printf("Reading file: %s\r\n", path);
+  File file = fs.open(path, "r");
+  if(!file || file.isDirectory()){
+    Serial.println("- empty file or failed to open file");
+    return String();
+  }
+  Serial.println("- read from file:");
+  String fileContent;
+  while(file.available()){
+    fileContent+=String((char)file.read());
+  }
+  Serial.println(fileContent);
+  return fileContent;
 }
 
-void writeFile(fs::FS &fs, const char *path, const char *message)
-{
-    Serial.printf("Writing file: %s\r\n", path);
-    File file = fs.open(path, "w");
-    if (!file)
-    {
-        //Serial.println("- failed to open file for writing");
-        return;
-    }
-    if (file.print(message))
-    {
-        Serial.println("- file written");
-    }
-    else
-    {
-        //Serial.println("- write failed");
-    }
+void writeFile(fs::FS &fs, const char * path, String message){
+ // Serial.printf("Writing file: %s\r\n", path);
+  File file = fs.open(path, "w");
+  if(!file){
+   // Serial.println("- failed to open file for writing");
+    return;
+  }
+  if(file.print(message)){
+    //Serial.println("- file written");
+  } else {
+    //Serial.println("- write failed");
+  }
 }
 
-void writeFile(fs::FS &fs, const char *path, String message)
-{
-    //Serial.printf("Writing file: %s\r\n", path);
-    File file = fs.open(path, "w");
-    if (!file)
-    {
-       // Serial.println("- failed to open file for writing");
-        return;
-    }
-    if (file.print(message))
-    {
-       // Serial.println("- file written");
-    }
-    else
-    {
-        //Serial.println("- write failed");
-    }
+void writeFile(fs::FS &fs, const char * path, const char * message){
+  Serial.printf("Writing file: %s\r\n", path);
+  File file = fs.open(path, "w");
+  if(!file){
+    Serial.println("- failed to open file for writing");
+    return;
+  }
+  if(file.print(message)){
+    Serial.println("- file written");
+  } else {
+    Serial.println("- write failed");
+  }
 }
-
 // Replaces placeholder with stored values
 String processor(const String &var)
 {
@@ -127,9 +109,9 @@ String processor(const String &var)
     {
         return readFile(SPIFFS, "/TEMP.txt");
     }
-    else if (var == "Encoder")
+    else if (var == "ENCODER")
     {
-        return readFile(SPIFFS, "/Encoder.txt");
+        return readFile(SPIFFS, "/ENCODER.txt");
     }
     else if (var =="MIN")
     {
@@ -139,5 +121,58 @@ String processor(const String &var)
     {
         return readFile(SPIFFS, "/HOUR.txt");
     }
+    else if (var == "inputString")
+    {
+        return readFile(SPIFFS, "/inputString.txt");
+    }
     return String();
+}
+
+void WiFiSetup(){
+    #ifdef ESP32
+  if (!SPIFFS.begin(true))
+  {
+    Serial.println("An Error has occurred while mounting SPIFFS");
+    return;
+  }
+#else
+  if (!SPIFFS.begin())
+  {
+    Serial.println("An Error has occurred while mounting SPIFFS");
+    return;
+  }
+#endif
+
+  Serial.begin(115200);
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  if (WiFi.waitForConnectResult() != WL_CONNECTED)
+  {
+    Serial.println("WiFi Failed!");
+    return;
+  }
+
+  Serial.println();
+  Serial.print("IP Address: ");
+  Serial.println(WiFi.localIP());
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request) {
+    request->send_P(200, "text/html", index_html, processor);
+  });
+  // Send a GET request to <ESP_IP>/get?ENC=<inputMessage>
+  server.on("/get", HTTP_GET, [](AsyncWebServerRequest *request) {
+    String inputMessage;
+ if (request->hasParam(parametr[5])) {
+      inputMessage = request->getParam(parametr[5])->value();
+      writeFile(SPIFFS, "/inputString.txt", inputMessage.c_str());
+    }
+    Serial.println(inputMessage);
+    request->send(200, "text/text", inputMessage);
+  });
+  server.begin();
+
+  // other adjust
+#ifndef ESP8266
+  while (!Serial)
+    ; // wait for serial port to connect. Needed for native USB
+#endif
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,9 @@
-/*********
-  Rui Santos
-  Complete project details at https://RandomNerdTutorials.com/esp32-esp8266-input-data-html-form/
-  
-  Permission is hereby granted, free of charge, to any person obtaining a copy
-  of this software and associated documentation files.
-  
-  The above copyright notice and this permission notice shall be included in all
-  copies or substantial portions of the Software.
-*********/
 
 #include <Arduino.h>
 #include <SPI.h>
 #include <OneWire.h>
+#define SCK D1
+#define DOUT D2
 #include <Wire.h>
 // encoder
 #include "GyverEncoder.h"
@@ -27,21 +19,16 @@ OneWire oneWire(ONE_WIRE_BUS);
 float tempC;
 DallasTemperature sensors(&oneWire);
 // oled screen
-#include <Adafruit_SSD1306.h>
-#include <Adafruit_GFX.h>
-#define SCREEN_WIDTH 128 // OLED display width, in pixels
-#define SCREEN_HEIGHT 32 // OLED display height, in pixels
-int variable = 15;
-#define OLED_RESET -1       // Reset pin # (or -1 if sharing Arduino reset pin)
-#define SCREEN_ADDRESS 0x3C ///< See datasheet for Address; 0x3D for 128x64, 0x3C for 128x32
-Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
-// RTC module
-#include "RTClib.h"
-RTC_DS1307 rtc;
 
+// RTC module
+int Watering_time;
 String process[6] = {"Irrigation", "Watering", "Mixing", "Drum rotation", "Waitting", "Emergency_off"};
 int current_sensor[5];
 int previous_sensor[5];
+String some;
+String inputParam;
+int ledState = LOW; // the current state of the output pin
+bool reading = 0;
 /*
 sensor[0] minute
 sensor[1] houre
@@ -49,52 +36,12 @@ sensor[2] temperature
 sensor[3] encoder
 sensor[4] button
 */
-bool flag;
+int flag;
 String current_procecc = "Waitting";
 
-// WIFI ADJUST 
-#ifdef ESP32
-  #include <WiFi.h>
-  #include <AsyncTCP.h>
-#else
-  #include <ESP8266WiFi.h>
-  #include <ESPAsyncTCP.h>
-#endif
-#include <ESPAsyncWebServer.h>
-
-AsyncWebServer server(80);
-
-// REPLACE WITH YOUR NETWORK CREDENTIALS
-const char* ssid = "Beeline_2G_F22959";
-const char* password = "dUNFcUEP";
-
-const char* PARAM_INPUT_1 = "input1";
-const char* PARAM_INPUT_2 = "input2";
-const char* PARAM_INPUT_3 = "input3";
-// HTML web page to handle 3 input fields (input1, input2, input3)
-const char index_html[] PROGMEM = R"rawliteral(
-<!DOCTYPE HTML><html><head>
-  <title>ESP Input Form</title>
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  </head><body>
-  <form action="/get">
-    input1: <input type="text" name="input1">
-    <input type="submit" value="Submit">
-  </form><br>
-  <form action="/get">
-    input2: <input type="text" name="input2">
-    <input type="submit" value="Submit">
-  </form><br>
-  <form action="/get">
-    input3: <input type="text" name="input3">
-    <input type="submit" value="Submit">
-  </form>
-</body></html>)rawliteral";
-
-void notFound(AsyncWebServerRequest *request) {
-  request->send(404, "text/plain", "Not found");
-}
-
+// WIFI ADJUST
+#include <WiFiAdjust.h>
+/*
 void oled_println(int *current_sensor, String *pro)
 {
   display.clearDisplay();
@@ -112,147 +59,90 @@ void oled_println(int *current_sensor, String *pro)
   display.display();
   Serial.println("OLED PRINT");
 }
-void setup()
+*/
+
+void MasterRequest()
 {
-Serial.begin(115200);
-WiFi.mode(WIFI_STA);
-  WiFi.begin(ssid, password);
-  if (WiFi.waitForConnectResult() != WL_CONNECTED) {
-    Serial.println("WiFi Failed!");
-    return;
+  Wire.beginTransmission(8);   /* Начинаем передачу на адресе 8 */
+  Wire.write("Hello Arduino"); /* Отправляем "hello Arduino" */
+  Wire.endTransmission();      /* прекращаем передачу */
+
+  Wire.requestFrom(8, 13); /* запрашиваем и считываем данные с 8 и 13 адреса slave устройства */
+  while (Wire.available())
+  {
+    int c = Wire.read();
+    Serial.print(c);
   }
   Serial.println();
-  Serial.print("IP Address: ");
-  Serial.println(WiFi.localIP());
-  // Send web page with input fields to client
-  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
-    request->send_P(200, "text/html", index_html);
-  });
-  // Send a GET request to <ESP_IP>/get?input1=<inputMessage>
-  server.on("/get", HTTP_GET, [] (AsyncWebServerRequest *request) {
-    String inputMessage;
-    String inputParam;
-    // GET input1 value on <ESP_IP>/get?input1=<inputMessage>
-    if (request->hasParam(PARAM_INPUT_1)) {
-      inputMessage = request->getParam(PARAM_INPUT_1)->value();
-      inputParam = PARAM_INPUT_1;
-    }
-    // GET input2 value on <ESP_IP>/get?input2=<inputMessage>
-    else if (request->hasParam(PARAM_INPUT_2)) {
-      inputMessage = request->getParam(PARAM_INPUT_2)->value();
-      inputParam = PARAM_INPUT_2;
-    }
-    // GET input3 value on <ESP_IP>/get?input3=<inputMessage>
-    else if (request->hasParam(PARAM_INPUT_3)) {
-      inputMessage = request->getParam(PARAM_INPUT_3)->value();
-      inputParam = PARAM_INPUT_3;
-    }
-    else {
-      inputMessage = "No message sent";
-      inputParam = "none";
-    }
-    Serial.println(inputMessage);
-    request->send(200, "text/html", "HTTP GET request sent to your ESP on input field (" 
-                                     + inputParam + ") with value: " + inputMessage +
-                                     "<br><a href=\"/\">Return to Home Page</a>");
-  });
-  server.onNotFound(notFound);
-  server.begin();
+  delay(1000);
+}
 
-  
-
-#ifndef ESP8266
-  while (!Serial)
-    ; // wait for serial port to connect. Needed for native USB
-#endif
-  //
+void setup()
+{
+  Serial.begin(9600);
+  // Initialize SPIFFS
+  WiFiSetup();
+  Wire.begin(D1, D2); /* задаем i2c мост через контакты SDA=D1 и SCL=D2 на NodeMCU */
   enc1.setType(TYPE2);
+  delay(500);
+  delay(500);
   sensors.begin();
-  if (!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS))
-  {
-    for (;;)
-      ; // Don't proceed, loop forever
-  }
-
-  // rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
-}
-
-void Irrigation()
-{
-}
-void Watering()
-{
-}
-void Mixing()
-{
-}
-void Drum_rotation()
-{
-}
-void Waitting()
-{
-}
-void Emergency_off()
-{
+  Serial.print("my fuck time  ");
 }
 
 void loop()
 {
-  
-  DateTime now = rtc.now();
-  if (current_sensor[0] != now.minute())
+  // To access
+  if (millis() - current_sensor[0] > 60000)
   {
-    current_sensor[0] = now.minute();
-    current_sensor[1] = now.hour();
+    current_sensor[0] = millis();
+    current_sensor[1] = 2;
     sensors.requestTemperatures(); // Send the command to get temperatures
     current_sensor[2] = sensors.getTempCByIndex(0);
-    Serial.println(current_sensor[0]);
-    Serial.println(current_sensor[1]);
-    Serial.println(current_sensor[2]);
+    writeFile(SPIFFS, "/MIN.txt", String(current_sensor[0]));
+    writeFile(SPIFFS, "/HOUR.txt", String(current_sensor[1]));
+    writeFile(SPIFFS, "/TEMP.txt", String(current_sensor[2]));
   }
 
   enc1.tick();
   if (enc1.isRight()) // encoder
   {
     current_sensor[3]++;
-    Serial.println(current_sensor[3]);
     if (current_sensor[3] > 5)
     {
       current_sensor[3] = 0;
     }
+    writeFile(SPIFFS, "/ENCODER.txt", String(current_sensor[3]));
   }
-
   if (enc1.isLeft())
   {
     current_sensor[3]--;
-    Serial.println(current_sensor[3]);
     if (current_sensor[3] < 1)
     {
       current_sensor[3] = 5;
     }
+    writeFile(SPIFFS, "/ENCODER.txt", String(current_sensor[3]));
+    //writeFile(SPIFFS, "/ENCODER.txt", "suck");
   }
-
   if (enc1.isPress()) // push button
   {
     current_sensor[4] = current_sensor[3];
-    Serial.println(current_sensor[4]);
   }
-
   for (int i = 0; i < 5; i++) // check if data changed
   {
     if (current_sensor[i] != previous_sensor[i])
     {
-      previous_sensor[i]=current_sensor[i];
+      previous_sensor[i] = current_sensor[i];
       Serial.println(current_sensor[i]);
       flag = 1;
-      
     }
   }
 
   if (flag)
   { // println data
-    oled_println(current_sensor, process);
+    // oled_println(current_sensor, process);
     flag = 0;
-    Serial.println("flag");
+    // Serial.println("flag");
   }
+  MasterRequest();
 }
